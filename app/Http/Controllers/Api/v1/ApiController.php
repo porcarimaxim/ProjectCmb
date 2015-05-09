bd5e16232357f6200bb3b4c21adcd736b5af4d11
Osoian Marcel <osoian.marcel.d@gmail.com>
2015-05-09 23:30:10 +0300

    Controllers doc.

--- app/Http/Controllers/Api/v1/ApiController.php
+++ app/Http/Controllers/Api/v1/ApiController.php
@@ -6,16 +6,21 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Response as IlluminateResponse;
 
-abstract class ApiController extends BaseController {
+abstract class ApiController extends BaseController
+{
 
 	use DispatchesCommands, ValidatesRequests;
 
 	/**
+	 * Respond status code
+	 *
 	 * @var int
 	 */
 	protected $statusCode = 200;
 
 	/**
+	 * Respond resource key
+	 *
 	 * @var string
 	 */
 	protected $resourceKey = 'data';
@@ -55,21 +60,46 @@ abstract class ApiController extends BaseController {
 		return $this;
 	}
 
+	/**
+	 * Return not found respond
+	 *
+	 * @param string $message
+	 * @return mixed
+	 */
 	public function respondNotFound($message = 'Resource not found')
 	{
 		return $this->setStatusCode(IlluminateResponse::HTTP_NOT_FOUND)->respondWithError($message);
 	}
 
+	/**
+	 * Return internal server error respond
+	 *
+	 * @param string $message
+	 * @return mixed
+	 */
 	public function respondInternalError($message = 'Internal server error')
 	{
 		return $this->setStatusCode(IlluminateResponse::HTTP_INTERNAL_SERVER_ERROR)->respondWithError($message);
 	}
 
+	/**
+	 * Return respond
+	 *
+	 * @param $data
+	 * @param array $headers
+	 * @return mixed
+	 */
 	private function respond($data, $headers = [])
 	{
 		return Response::json($data, $this->getStatusCode(), $headers);
 	}
 
+	/**
+	 * Return error respond
+	 *
+	 * @param $message
+	 * @return mixed
+	 */
 	private function respondWithError($message)
 	{
 		return $this->respond([
@@ -80,7 +110,15 @@ abstract class ApiController extends BaseController {
 		]);
 	}
 
-	public function respondDestroy($id, $message = 'Resource deleted') {
+	/**
+	 * Return destroy respond
+	 *
+	 * @param $id
+	 * @param string $message
+	 * @return mixed
+	 */
+	public function respondDestroy($id, $message = 'Resource deleted')
+	{
 		return $this->respond([
 			$this->getResourceKey() => [
 				'id' => $id,
